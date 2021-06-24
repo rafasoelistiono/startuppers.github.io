e1ba3703ed5ff2376fc0402ebb258bb3d6c24adb
rafasoelistiono <71561783+rafasoelistiono@users.noreply.github.com>
2021-06-24 14:13:58 +0700

    Add files via upload

---
+++ layanan-bisnis/layanan.js
@@ -0,0 +1,15 @@
+window.onload=function(){
+    setTimeout(img, 1000);
+}
+
+function img(){
+    var z=document.querySelector('#reveal');
+    z.style.opacity='1';
+    z.style.transform='translate(0)';
+    var y=document.querySelector('#reveal1');
+    y.style.opacity='1';
+    y.style.transform='translate(0)';
+    var x=document.querySelector('#reveal2');
+    x.style.opacity='1';
+    x.style.transform='translate(0)';
+}
